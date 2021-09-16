3ebf0f3d16ae05fad5b71ce8bb85de35dfca9468
sanaul01 <mdsanaulislam154@gmail.com>
2021-09-16 13:42:29 +0600

    desine

--- app.js
+++ app.js
@@ -1,24 +1,39 @@
 const loadData = () =>{
     const searchText = document.getElementById('input-field')
     const inputText = searchText.value;
-    console.log(inputText)
+    searchText.value = '';
+    error.textContent = ''
+    
+    if(inputText === ''){
+      const error = document.getElementById('error');
+      error.textContent = '';
+      const p = document.createElement('p')
+      p.innerText = `error`
+      error.appendChild(p)
+    }
+    
+    
+    else {
+      const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputText}`;
+     fetch(url)
+     .then(resposive => resposive.json())
+     .then(data => displayField(data.meals))
 
+    }
+  }
+    
 
-const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputText}`
-fetch(url)
-.then(resposive => resposive.json())
-.then(data => displayField(data.meals))
-
-}
 const displayField = meals =>{
+ 
     const displayField = document.getElementById('display-field')
+    displayField.textContent = '';
     meals.forEach(meal =>{
         console.log(meal)
         const div = document.createElement('div')
         div.classList.add('card')
         div.innerHTML = `
         <div class="col">
-        <div class="card h-100">
+        <div onclick="details(${meal.idMeal})" class="card h-100">
           <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title">Title: ${meal.strMeal}</h5>
@@ -29,4 +44,36 @@ const displayField = meals =>{
         `;
         displayField.appendChild(div)
     });
+    
+}
+
+const details = mealId =>{
+  console.log(mealId)
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`
+  fetch(url)
+  .then(res => res.json())
+  .then(data => displayDetails(data.meals))
+}
+
+const displayDetails = details =>{
+  
+  
+  details.forEach(detail =>{
+    const foodDetails = document.getElementById('detail')
+    foodDetails.textContent = '';
+    const div = document.createElement('div')
+  div.classList.add('card')
+  div.innerHTML = `
+  <div class="card">
+          <img src="${detail.strMealThumb}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Title: ${detail.strMeal}</h5>
+            <p class="card-text">Details: ${detail.strInstructions.slice(0, 100)}.</p>
+            <a href="${detail.strYoutube}" class="btn btn-primary">Show me</a>
+          </div>
+  </div>
+  `;
+  foodDetails.appendChild(div)
+  
+  })
 }
